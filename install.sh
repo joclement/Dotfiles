74936e25c64e1031d9add76655c3d3a1ddd69e2c
flyingdutchman23 <flyingdutchman@posteo.eu>
2017-02-05 16:41:07 +0100

    Cleanup.

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@
 ### Tested with Ubuntu! So not recommended for other operating systems without prior check
 ### and change!
 #
-### Prior to running the first time I would recommend to save the Dotfiles before, 
+### Prior to running the first time I would recommend to save the Dotfiles before,
 ### just in they get lost!
 #
 # This script sets up the Dotfiles environment from github.com/flyingdutchman23/Dotfiles.
@@ -49,8 +49,8 @@ olddir=$to_dots/Dotfiles_old
 
 # list of files/folders to symlink in homedir
 files="bashrc vimrc gitconfig shared_aliases zshrc shared_shell\
-	dircolors-solarized antigen vim/bundle/Vundle.vim vim/ftplugin/* env\
-	autocompletion_zsh config/zathura/zathurarc"
+    dircolors-solarized antigen vim/bundle/Vundle.vim vim/ftplugin/* env\
+    autocompletion_zsh config/zathura/zathurarc"
 
 dirs="vim/bundle vim/ftplugin config/zathura"
 
@@ -64,338 +64,357 @@ changedefzsh=true
 
 #Help function
 function HELP {
-echo -e \\n"Help documentation for ${script}"\\n
-echo "Installs the dotfiles of this git Dotfiles repository. You may need to give your passport
-to install needed sytem wide software packages, f.x. cmake"
-echo -e "Basic usage: ./$script "\\n
-echo -e "-d    specify parent dir of Dotfiles git repo. Dotfiles has to be in that dir. 
-Folder needs to exist. 
-Normally this folder is the home directory, f.x. .vimrc is normally stored in $HOME/.vimrc. 
-Default value is $to_dots . 
-Folder must not have a ending /"\\n
-echo -e "-i    gives an installoption, necessary option. 
-Use either \"update\" or \"install\" as an argument." \\n
-echo -e "-y    recompile YouCompleteMe. Default is $recompileyoucompleteme" \\n
-echo -e "-s    Install some parts system wide. Default is $system_wide" \\n
-echo -e "-n    Do not set zsh as default shell" \\n
-echo -e "-h    Displays this help message. No further functions are performed."\\n
-echo -e "Example for installation: $script -i install -y"\\n
-echo -e "Example for update: $script -i update -y"\\n
-exit 1
+    echo -e \\n"Help documentation for ${script}"\\n
+    echo "Installs the dotfiles of this git Dotfiles repository. You may need to give your passport
+    to install needed sytem wide software packages, f.x. cmake"
+    echo -e "Basic usage: ./$script "\\n
+    echo -e "-d    specify parent dir of Dotfiles git repo. Dotfiles has to be in that dir.
+    Folder needs to exist.
+    Normally this folder is the home directory, f.x. .vimrc is normally stored in $HOME/.vimrc.
+    Default value is $to_dots .
+    Folder must not have a ending /"\\n
+    echo -e "-i    gives an installoption, necessary option.
+    Use either \"update\" or \"install\" as an argument." \\n
+    echo -e "-y    recompile YouCompleteMe. Default is $recompileyoucompleteme" \\n
+    echo -e "-s    Install some parts system wide. Default is $system_wide" \\n
+    echo -e "-n    Do not set zsh as default shell" \\n
+    echo -e "-h    Displays this help message. No further functions are performed."\\n
+    echo -e "Example for installation: $script -i install -y"\\n
+    echo -e "Example for update: $script -i update -y"\\n
+    exit 1
 }
 
 #check if bash is used
-if [[ 0 == 0 ]]; then
-
-	echo "start installation"
+if [[ 0 == 0 ]];
+then
+    echo ""
 else
-	echo "You need to use bash to run this script!"
-	exit 1
+    echo "You need to use bash to run this script!"
+    exit 1
 fi
 
 while getopts ":d:i:s y n h" opt; do
-	case $opt in
-		d)
-			if [ -d "$OPTARG" ]; then
-				if [[ -L "$OPTARG" ]]; then
-					echo "specified directory is a link. EXIT!"
-					exit 1
-				else
-					to_dots=$OPTARG
-				fi
-			else
-				echo "specified directory does not exist. EXIT!"
-				HELP;
-				exit 1
-			fi
-			;;
-		i)
-			if [ "$OPTARG" = "update" -o "$OPTARG" = "install" ]; then
-				installoption="$OPTARG"
-			else
-				echo "$OPTARG is not a valid argument. EXIT!"
-				HELP;
-				exit 1
-
-			fi
-			;;
-		s)
-			system_wide=true
-			;;
-		y)
-			recompileyoucompleteme=true
-			;;
-		n)
-			changedefzsh=false
-			;;
-		h)
-			HELP;
-			;;
-		\?)
-			echo "Invalid option:
-			-$OPTARG" >&2
-			HELP;
-			;;
-	esac
+    case $opt in
+        d)
+            if [ -d "$OPTARG" ];
+            then
+                if [[ -L "$OPTARG" ]];
+                then
+                    echo "specified directory is a link. EXIT!"
+                    exit 1
+                else
+                    to_dots=$OPTARG
+                fi
+            else
+                echo "specified directory does not exist. EXIT!"
+                HELP;
+                exit 1
+            fi
+            ;;
+        i)
+            if [ "$OPTARG" = "update" -o "$OPTARG" = "install" ];
+            then
+                installoption="$OPTARG"
+            else
+                echo "$OPTARG is not a valid argument. EXIT!"
+                HELP;
+                exit 1
+            fi
+            ;;
+        s)
+            system_wide=true
+            ;;
+        y)
+            recompileyoucompleteme=true
+            ;;
+        n)
+            changedefzsh=false
+            ;;
+        h)
+            HELP;
+            ;;
+        \?)
+            echo "Invalid option:
+            -$OPTARG" >&2
+            HELP;
+            ;;
+    esac
 done
 
 
 create_olddir() {
 
-	# create dotfiles_old in homedir
-	echo -n "Creating $olddir for backup of any existing dotfiles in $my_home ..."
-	#TODO check if the existing is a directory
-	if [ ! -f "$my_home/$olddir" ]; then
-		mkdir -p $olddir
-	else
-		echo "There is a file namen $olddir, which is supposed to be
-		the olddir file"
-		exit
-	fi
-	echo "done"
+    # create dotfiles_old in homedir
+    echo -n "Creating $olddir for backup of any existing dotfiles in $my_home ..."
+    #TODO check if the existing is a directory
+    if [ ! -f "$my_home/$olddir" ];
+    then
+        mkdir -p $olddir
+    else
+        echo "There is a file namen $olddir, which is supposed to be
+        the olddir file"
+        exit
+    fi
+    echo "done"
 }
 
 
 symlink_files() {
 
-	# move any existing dotfiles in homedir to dotfiles_old directory, 
-	#then create symlinks from the homedir to any files in the ~/dotfiles directory specified in $files
+    # move any existing dotfiles in homedir to dotfiles_old directory,
+    #then create symlinks from the homedir to any files in the ~/dotfiles directory specified in $files
 
-        for dir in $dirs; do
-            echo "Create dotfile dir: $dir"
-            if [ ! -f "$my_home/.$dir" ]; then
-                mkdir -p $my_home/.$dir
-            else
-                    echo "There is a file named $dir, which is supposed to be
-                    a directory"
-                    exit
-            fi
-        done
-	for file in $files; do
-		echo "Moving any existing dotfiles from $my_home to $olddir"
-		if [ -f "$my_home/.$file" ]; then
-			echo "Move $file into $olddir"
-			mv $my_home/.$file $olddir
-		elif [ -d "$my_home/.$file" ]; then 
-			echo "This dir $file should be a file. EXIT!"
-			exit
-		else
-			rm -f $my_home/.$file
-		fi
-		echo "done"
-		echo "Creating symlink to $file in $my_home directory."
-		ln -s $dot_dir/$file $my_home/.$file
-		echo "done"
-	done
+    for dir in $dirs; do
+        echo "Create dotfile dir: $dir"
+        if [ ! -f "$my_home/.$dir" ];
+        then
+            mkdir -p $my_home/.$dir
+        else
+            echo "There is a file named $dir, which is supposed to be
+            a directory"
+            exit
+        fi
+    done
+    for file in $files; do
+        echo "Moving any existing dotfiles from $my_home to $olddir"
+        if [ -f "$my_home/.$file" ];
+        then
+            echo "Move $file into $olddir"
+            mv $my_home/.$file $olddir
+        elif [ -d "$my_home/.$file" ];
+        then
+            echo "This dir $file should be a file. EXIT!"
+            exit
+        else
+            rm -f $my_home/.$file
+        fi
+        echo "done"
+        echo "Creating symlink to $file in $my_home directory."
+        ln -s $dot_dir/$file $my_home/.$file
+        echo "done"
+    done
 }
 
 install_dependencies() {
 
-	platform=$(uname);
-	if [[ $platform == 'Linux' ]]; then
-		if [[ -f /etc/debian_version ]]; then
+    platform=$(uname);
+    if [[ $platform == 'Linux' ]];
+    then
+        if [[ -f /etc/debian_version ]];
+        then
 
-			echo "check if pip3 is installed, if not install"
-			if [ $(dpkg-query -l | grep pip3 | wc -l) == 0 ];
-			then
-				sudo apt-get install python3-pip;
-			fi
-			echo "done"
+            echo "check if pip3 is installed, if not install"
+            if [ $(dpkg-query -l | grep pip3 | wc -l) == 0 ];
+            then
+                sudo apt-get install python3-pip;
+            fi
+            echo "done"
 
-			echo "check if build-essential is installed, if not install"
-			if [ $(dpkg-query -l | grep build-essential | wc -l) == 0 ];
-			then
-				sudo apt-get install build-essential;
-			fi
-			echo "done"
+            echo "check if build-essential is installed, if not install"
+            if [ $(dpkg-query -l | grep build-essential | wc -l) == 0 ];
+            then
+                sudo apt-get install build-essential;
+            fi
+            echo "done"
 
-			echo "check if cmake is installed, if not install"
-			if [ $(dpkg-query -l | grep cmake | wc -l) == 0 ];
-			then
-				sudo apt-get install cmake;
-			fi
-			echo "done"
+            echo "check if cmake is installed, if not install"
+            if [ $(dpkg-query -l | grep cmake | wc -l) == 0 ];
+            then
+                sudo apt-get install cmake;
+            fi
+            echo "done"
 
-			echo "check if python-dev is installed, if not install"
-			if [ $(dpkg-query -l | grep python-dev | wc -l) == 0 ];
-			then
-				sudo apt-get install python-dev;
-			fi
-			echo "done"
+            echo "check if python-dev is installed, if not install"
+            if [ $(dpkg-query -l | grep python-dev | wc -l) == 0 ];
+            then
+                sudo apt-get install python-dev;
+            fi
+            echo "done"
 
-			echo "check if vim is installed, if not install"
-			if [ $(dpkg-query -l | grep vim | wc -l) == 0 ];
-			then
-				sudo apt-get install vim;
-			fi
-			echo "done"
+            echo "check if vim is installed, if not install"
+            if [ $(dpkg-query -l | grep vim | wc -l) == 0 ];
+            then
+                sudo apt-get install vim;
+            fi
+            echo "done"
 
-			echo "check if git is installed, if not install"
-			if [ $(dpkg-query -l | grep git | wc -l) == 0 ];
-			then
-				sudo apt-get install git;
-			fi
-			echo "done"
+            echo "check if git is installed, if not install"
+            if [ $(dpkg-query -l | grep git | wc -l) == 0 ];
+            then
+                sudo apt-get install git;
+            fi
+            echo "done"
 
-			echo "check if dconf-cli is installed, if not install"
-			if [ $(dpkg-query -l | grep dconf-cli | wc -l) == 0 ];
-			then
-				sudo apt-get install dconf-cli;
-			fi
-			echo "done"
-		fi
-	elif [[ $platform == 'Darwin' ]]; then
-		echo "This script might not be suitable for MAC OS, but you can
-		try!"
-		exit
-	else 
-		echo "Platform $platform not known!"
-		echo "This script might be not suitable for your platform"
-		exit
-	fi
+            echo "check if dconf-cli is installed, if not install"
+            if [ $(dpkg-query -l | grep dconf-cli | wc -l) == 0 ];
+            then
+                sudo apt-get install dconf-cli;
+            fi
+            echo "done"
+        fi
+    elif [[ $platform == 'Darwin' ]];
+    then
+        echo "This script might not be suitable for MAC OS, but you can
+        try!"
+        exit
+    else
+        echo "Platform $platform not known!"
+        echo "This script might be not suitable for your platform"
+        exit
+    fi
 }
 
 default_zsh() {
 
-	if [ "$changedefzsh" = "true" ]; then
-		# Set the default shell to zsh if it isn't currently set to zsh
-		if [[ ! $(echo $SHELL) == $(which zsh) ]]; then
-			echo "change default shell to zsh..."
-			chsh -s $(which zsh)
-			echo "done"
-		fi
-	fi
+    if [ "$changedefzsh" = "true" ];
+    then
+        # Set the default shell to zsh if it isn't currently set to zsh
+        if [[ ! $(echo $SHELL) == $(which zsh) ]];
+        then
+            echo "change default shell to zsh..."
+            chsh -s $(which zsh)
+            echo "done"
+        fi
+    fi
 }
 
 install_zsh () {
-	# Test to see if zshell is installed.  If it is:
-	if [ -f /bin/zsh -o -f /usr/bin/zsh ]; then
-		default_zsh
-	else
-		# If zsh isn't installed, get the platform of the current machine
-		platform=$(uname);
-		# If the platform is Linux, try an apt-get to install zsh and then recurse
-		if [[ $platform == 'Linux' ]]; then
-			if [[ -f /etc/redhat-release ]]; then
-				sudo yum install zsh
-				install_zsh
-			fi
-			if [[ -f /etc/debian_version ]]; then
-				sudo apt-get install zsh
-				install_zsh
-			fi
+    # Test to see if zshell is installed.  If it is:
+    if [ -f /bin/zsh -o -f /usr/bin/zsh ];
+    then
+        default_zsh
+    else
+        # If zsh isn't installed, get the platform of the current machine
+        platform=$(uname);
+        # If the platform is Linux, try an apt-get to install zsh and then recurse
+        if [[ $platform == 'Linux' ]];
+        then
+            if [[ -f /etc/redhat-release ]];
+            then
+                sudo yum install zsh
+                install_zsh
+            fi
+            if [[ -f /etc/debian_version ]];
+            then
+                sudo apt-get install zsh
+                install_zsh
+            fi
 
-			# If the platform is OS X, tell the user to install zsh :)
-		elif [[ $platform == 'Darwin' ]]; then
-			echo "Please install zsh, then re-run this script!"
-			exit
-		fi
-	fi
+            # If the platform is OS X, tell the user to install zsh :)
+        elif [[ $platform == 'Darwin' ]];
+        then
+            echo "Please install zsh, then re-run this script!"
+            exit
+        fi
+    fi
 }
 
 install_powerline() {
 
-	echo "check if powerline is installed, if not install"
-	if [ $(pip3 list | grep powerline-status | wc -l) == 0 ];
-	then
-		if [ "$system_wide" = true ]; then
-			echo "installing system wide"
-			sudo pip3 install powerline-status;
-		else
-			echo "installing for user"
-			pip3 install --user powerline-status;
-		fi
-	fi
-	echo "done"
+    echo "check if powerline is installed, if not install"
+    if [ $(pip3 list | grep powerline-status | wc -l) == 0 ];
+    then
+        if [ "$system_wide" = true ];
+        then
+            echo "installing system wide"
+            sudo pip3 install powerline-status;
+        else
+            echo "installing for user"
+            pip3 install --user powerline-status;
+        fi
+    fi
+    echo "done"
 
-	echo "install powerline fonts"
+    echo "install powerline fonts"
 
-	wget https://github.com/powerline/powerline/raw/develop/font/PowerlineSymbols.otf;
-	wget https://github.com/powerline/powerline/raw/develop/font/10-powerline-symbols.conf;
-	if [ "$system_wide" = true ]; then
-		echo "installing system wide..."
-		echo "move PowerlineSymbols to /usr/share/fonts/"
-		sudo mv PowerlineSymbols.otf /usr/share/fonts/
-		echo "update fonts cache"
-		sudo fc-cache -vf
-		echo "move fonts conf to /etc/fonts/conf.d/"
-		sudo mv 10-powerline-symbols.conf /etc/fonts/conf.d/
-	else
-		echo "installing for user..."
-		echo "move PowerlineSymbols to ~/.fonts/, create folder if non-existing"
-		mkdir -p ~/.fonts/
-		mv PowerlineSymbols.otf ~/.fonts/
-		echo "update fonts cache"
-		fc-cache -vf ~/.fonts/
-		echo "move fonts conf to ~/.config/fontconfig/conf.d/, create folder if non-existing"
-		mkdir -p ~/.config/fontconfig/conf.d/
-		mv 10-powerline-symbols.conf ~/.config/fontconfig/conf.d/
-	fi
-	echo "done"
+    wget https://github.com/powerline/powerline/raw/develop/font/PowerlineSymbols.otf;
+    wget https://github.com/powerline/powerline/raw/develop/font/10-powerline-symbols.conf;
+    if [ "$system_wide" = true ];
+    then
+        echo "installing system wide..."
+        echo "move PowerlineSymbols to /usr/share/fonts/"
+        sudo mv PowerlineSymbols.otf /usr/share/fonts/
+        echo "update fonts cache"
+        sudo fc-cache -vf
+        echo "move fonts conf to /etc/fonts/conf.d/"
+        sudo mv 10-powerline-symbols.conf /etc/fonts/conf.d/
+    else
+        echo "installing for user..."
+        echo "move PowerlineSymbols to ~/.fonts/, create folder if non-existing"
+        mkdir -p ~/.fonts/
+        mv PowerlineSymbols.otf ~/.fonts/
+        echo "update fonts cache"
+        fc-cache -vf ~/.fonts/
+        echo "move fonts conf to ~/.config/fontconfig/conf.d/, create folder if non-existing"
+        mkdir -p ~/.config/fontconfig/conf.d/
+        mv 10-powerline-symbols.conf ~/.config/fontconfig/conf.d/
+    fi
+    echo "done"
 }
 
 update_vim() {
 
-	echo "install vim add ons"
-	vim +PluginUpdate +qall
-	echo "done"
+    echo "install vim add ons"
+    vim +PluginUpdate +qall
+    echo "done"
 
-	if [ "$recompileyoucompleteme" = "true" ]; then
-		echo "install YouCompleteMe if it is a addon"
-		if [ -f $my_home/.vim/bundle/YouCompleteMe/install.py ]; then
-			python $my_home/.vim/bundle/YouCompleteMe/install.py --clang-completer
-		else 
-			echo "YouCompleteMe has not been found"
-		fi
-		echo "done"
-	fi
+    if [ "$recompileyoucompleteme" = "true" ];
+    then
+        echo "install YouCompleteMe if it is a addon"
+        if [ -f $my_home/.vim/bundle/YouCompleteMe/install.py ];
+        then
+            python $my_home/.vim/bundle/YouCompleteMe/install.py --clang-completer
+        else
+            echo "YouCompleteMe has not been found"
+        fi
+        echo "done"
+    fi
 }
 
 install_solarized() {
-	cd $dot_dir/gnome-terminal-colors-solarized/
-	. install.sh
-	cd
+    cd $dot_dir/gnome-terminal-colors-solarized/
+    . install.sh
+    cd
 }
 
 dorequirements() {
-	install_dependencies
-	git submodule update --recursive --init 
-	install_zsh
+    install_dependencies
+    git submodule update --recursive --init
+    install_zsh
 }
 
 backup_link() {
-	# creates sometimes error, does not have a real benefit
-	create_olddir
-	symlink_files
+    create_olddir
+    symlink_files
 }
 
 doinstalls() {
-	echo "install solarized..."
-	install_solarized
-	echo "finish installtion solarized"
-	install_powerline
-	update_vim
+    echo "install solarized..."
+    install_solarized
+    echo "finish installtion solarized"
+    install_powerline
+    update_vim
 }
 
 completeinstallation() {
-	dorequirements
-	backup_link
-	doinstalls
+    dorequirements
+    backup_link
+    doinstalls
 }
 
 update() {
-
-	git submodule update --recursive --init 
-	backup_link
-	update_vim
+    git submodule update --recursive --init
+    backup_link
+    update_vim
 }
 
 install() {
-
-	if [ "$installoption" = "update" ]; then
-		update
-	elif [ "$installoption" = "install" ]; then
-		completeinstallation
-	fi
+    if [ "$installoption" = "update" ];
+    then
+        update
+    elif [ "$installoption" = "install" ];
+    then
+        completeinstallation
+    fi
 }
 install
