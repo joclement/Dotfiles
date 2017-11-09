de20b08995f754658cc36cb6abc0b2b39bf8562e
Joris Clement <JorisClement@posteo.de>
2017-11-09 23:34:42 +0100

    To have a self-controlled, stable version of vim-plug.
    Otherwise there might be strange behavior, because vim-plug has
    updated between 2 installs.

--- install.sh
+++ install.sh
@@ -55,7 +55,8 @@ olddir=$to_dots/Dotfiles_old
 # list of files/folders to symlink in homedir
 files="bashrc vimrc gvimrc gitconfig shared_aliases zshrc inputrc shared_shell\
        dircolors-solarized antigen vim/ftplugin/* vim/syntax/* vim/ftdetect/*\
-       env autocompletion_zsh config/zathura/zathurarc tmux.conf hgrc hgignore"
+       env autocompletion_zsh config/zathura/zathurarc tmux.conf hgrc hgignore
+       vim/autoload/*"
 
 dirs="vim/autoload vim/ftplugin vim/plugin vim/syntax vim/ftdetect config/zathura"
 
