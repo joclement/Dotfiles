738b777b127ac54ccf90fd977e30b0a8c3c466b1
flyingdutchman <flyingdutchman@posteo.eu>
2022-02-18 14:40:19 +0100

    Change license
    Set the MIT license as the license for nearly all files. The GPLv3
    license only applies to the file `./install.sh` as it is based on a file
    from a different author. This is stated in the Readme.md.

--- install.sh
+++ install.sh
@@ -1,6 +1,27 @@
 #!/bin/bash
-############################
+
+# The license of this file is different than the others because it is based on
+# content from the following repository:
+# https://github.com/michaeljsmalley/dotfiles
 #
+############################
+# Copyright (C) 2013  Michael J. Smalley
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+############################
+
+############################
 ### Tested with Ubuntu! So not recommended for other operating systems without prior check
 ### and change!
 #
