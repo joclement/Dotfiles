edb46891f418bc8530c24f0129e5ef5aecc0120e
Joris Clement <7713214+joclement@users.noreply.github.com>
2023-07-19 17:17:08 +0200

    feat!: fix CI
    Drop the GitHub CLI tool from this repository for the following reasons:
    - Fix the CI, the GPG key is probably outdated or there is some
      temporary problem
    - This tool fits better into my DevPackages repository as it is not
      directly related to dotfiles
    
    MR/commit https://github.com/joclement/DevPackages/pull/5/commits/d24c51fe5148a317ad3a900e39f7a1f3fd66cc9f
    will integrate the tool there.

--- install.sh
+++ install.sh
@@ -137,14 +137,6 @@ install_coc_dependencies() {
     sudo npm install --global yarn
 }
 
-install_github_cli() {
-    curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo dd of=/usr/share/keyrings/githubcli-archive-keyring.gpg \
-    && sudo chmod go+r /usr/share/keyrings/githubcli-archive-keyring.gpg \
-    && echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null \
-    && sudo apt-get update \
-    && sudo apt-get install gh -y
-}
-
 install_powerline() {
     echo "install powerline-status"
     pip3 install powerline-status
@@ -173,7 +165,6 @@ install_solarized() {
 
 doinstalls() {
     install_solarized
-    install_github_cli
     install_coc_dependencies
     install_powerline
     update_vim
