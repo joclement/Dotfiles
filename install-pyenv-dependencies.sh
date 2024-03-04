2da618b1cd8ac21d388710f4e45cb4bf916d9b09
Joris Clement <7713214+joclement@users.noreply.github.com>
2024-03-04 22:22:11 +0100

    refactor: drop comment

---
+++ install-pyenv-dependencies.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+set -euo pipefail
+
+sudo apt-get install -y \
+    libedit-dev \
+    libssl-dev \
+    zlib1g-dev \
+    libbz2-dev \
+    libreadline-dev \
+    libsqlite3-dev \
+    llvm \
+    libncursesw5-dev \
+    xz-utils \
+    tk-dev \
+    libxml2-dev \
+    libxmlsec1-dev \
+    libffi-dev \
+    liblzma-dev
