d359a7da69084632d0ea45567f63629a041dcc35
Joris Clement <7713214+joclement@users.noreply.github.com>
2024-05-27 17:19:45 +0200

    feat: add workaround for Ubuntu 24.04
    Signed-off-by: Joris Clement <7713214+joclement@users.noreply.github.com>

--- install-python-build-dependencies.sh
+++ install-python-build-dependencies.sh
@@ -13,10 +13,20 @@ sudo apt-get install --yes \
     liblzma-dev \
     libncursesw5-dev \
     libreadline-dev \
-    libsqlite3-dev \
-    libssl-dev \
-    libxml2-dev \
-    libxmlsec1-dev \
-    tk-dev \
-    xz-utils \
-    zlib1g-dev
+    libsqlite3-dev
+
+os_version=$(lsb_release -sr)
+if [[ $os_version == "24.04" ]]; then
+    # TODO address problem after upgrading to 24.04
+    echo \
+        "ERROR: Installing all Python build dependencies fails" \
+        " in Github Actions for Ubuntu 24.04"
+else
+    sudo apt-get install --yes \
+        libssl-dev \
+        libxml2-dev \
+        libxmlsec1-dev \
+        tk-dev \
+        xz-utils \
+        zlib1g-dev
+fi
