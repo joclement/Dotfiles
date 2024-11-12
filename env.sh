a6b44aa934d0f0f5abce11a4583a87b7368717a4
Joris Clement <7713214+joclement@users.noreply.github.com>
2024-11-12 23:53:23 +0100

    feat!: drop pyenv and add deadsnakes ppa to https://github.com/joclement/DevPackages
    Close #184.
    
    Signed-off-by: Joris Clement <7713214+joclement@users.noreply.github.com>

--- env.sh
+++ env.sh
@@ -1,14 +1,6 @@
 ###This file contains the environment variables used by bash and zsh
 
-# Functions to add a folder to the PATH environment variable:
-# Both functions take 1 argument, namely the dir, which should be added.
-# this function adds the dir to the begin, if the dir is not in PATH
-__pathaddbegin() {
-    if [ -d "$1" ] && [[ ":$PATH" != *":$1:"* ]]; then
-        PATH="$1${PATH:+":$PATH"}"
-    fi
-}
-# add dir to end, if it is nowhere in PATH
+# NOTE: only add the directory if it is nowhere in PATH
 __pathaddend() {
     if [ -d "$1" ] && [[ ":$PATH:" != *":$1:"* ]]; then
         PATH="$PATH:$1"
@@ -28,18 +20,11 @@ if [ -d "$POETRY_HOME" ]; then
     __pathaddend "$POETRY_HOME"
 fi
 
-export PYENV_ROOT="$HOME/.pyenv"
-if [ -d "$PYENV_ROOT/bin" ]; then
-    __pathaddbegin "$PYENV_ROOT"/bin
-    __pathaddbegin "$PYENV_ROOT"/shims
-fi
-
 if [ -f "$HOME/.cargo/env" ]; then
     # shellcheck source=/dev/null
     source "$HOME"/.cargo/env
 fi
 
-unset -f __pathaddbegin
 unset -f __pathaddend
 
 # ---------------------------------------------------------------------
