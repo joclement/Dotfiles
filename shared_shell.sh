ab9b22ee6d6d7f4fd6105df96244ba05eeccf70b
Joris Clement <7713214+joclement@users.noreply.github.com>
2025-01-31 17:41:28 +0100

    feat: add autocompletion for `uv`
    Signed-off-by: Joris Clement <7713214+joclement@users.noreply.github.com>

--- shared_shell.sh
+++ shared_shell.sh
@@ -1,4 +1,5 @@
 # This file contains all the settings shared between different shells(bash, zsh)
+# shellcheck disable=SC2046,SC2086
 
 # shellcheck source=shared_shell.sh
 source "$HOME"/.shared_aliases.sh
@@ -13,5 +14,6 @@ fi
 eval "$(dircolors ~/.dircolors-solarized/dircolors.ansi-dark)"
 
 export DIRENV_LOG_FORMAT=""
-# shellcheck disable=SC2046,SC2086
 eval "$(direnv hook $(basename $SHELL))"
+
+eval "$(uv generate-shell-completion $(basename $SHELL))"
